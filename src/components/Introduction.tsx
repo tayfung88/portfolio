a6745898b659bb1965cb637ca7fa1436a00090bf
tayfung88 <tayfungg88@gmail.com>
2024-08-10 11:40:39 +0200

    wip

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -15,7 +15,7 @@ const Introduction: React.FC = () => {
         </div>
         <div className="w-full md:w-5/12 flex justify-center md:justify-end mb-8 md:mb-0">
     <img
-        src="./public/images/pic.jpg"
+        src="./images/pic.jpg"
         alt="Photo Tayfun GUGLU"
         className="w-full max-w-[10rem] md:max-w-[15rem] lg:max-w-[10rem] rounded-full shadow-xl border-4 border-transparent transition-all duration-500 hover:border-gray-600 hover:shadow-2xl hover:scale-105"
     />
